7e937cf0a7251d416c1c5dc5b48e0422ec96eed5
longhoang94 <longsutls@gmail.com>
2018-11-23 21:54:27 +0700

    add DrugServiceImpl implement Service

--- src/main/java/com/longhoang/services/impl/DrugServiceImpl.java
+++ src/main/java/com/longhoang/services/impl/DrugServiceImpl.java
@@ -1,4 +1,30 @@
 package com.longhoang.services.impl;
 
-public class DrugServiceImpl {
+import com.longhoang.models.Drug;
+import com.longhoang.repository.DrugRepository;
+import com.longhoang.services.DrugService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class DrugServiceImpl implements DrugService {
+    private DrugRepository drugRepository;
+    @Override
+    public Page<Drug> findAll(Pageable pageable) {
+        return drugRepository.findAll(pageable);
+    }
+
+    @Override
+    public Drug findById(Long id) {
+        return drugRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Drug drug) {
+        drugRepository.save(drug);
+    }
+
+    @Override
+    public void remove(Long id) {
+        drugRepository.delete(id);
+    }
 }
